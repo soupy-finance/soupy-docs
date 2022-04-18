6cc8c0242fa8859b113d72a42138338efabfeea5
soupy-spook <spook@soupy.finance>
2022-04-18 16:59:26 -0700

    first commit

---
+++ blog/2022-04-18-welcome/index.md
@@ -0,0 +1,26 @@
+---
+slug: welcome
+title: Welcome
+authors: [spook]
+tags: [hello, welcome, soupy]
+---
+
+Welcome to Soupy Finance! We are a new DeFi project looking to bring cutting edge technology to the decentralized exchange space. 
+
+Despite the many advantages offered by decentralized exchanges, centralized exchanges still reign supreme for a few key reasons and features. Several of these include
+- Easy fiat on and offboarding
+- Chain agnosticism 
+- Transaction execution speed
+- Order books
+- Deep liquidity with low spreads
+- Advanced trading features
+- Perpetual futures and other derivatives 
+
+Soupy aims to bring all of these listed features and more to create a novel experience that can rival even the best of centralized exchanges, while offering all of the benefits of decentralization. More details will be coming shortly, but for now the architecture consits of
+- A custom Cosmos chain to facilitate blazing fast transactions, oracles, bridging, and relay features
+- Contracts on an ever growing listed of supported chains (Ethereum, Avalanche, Solana, Terra, BSC, etc.) for cross-chain liquidity, bridging, and trading
+- A fiat on and offramp complete with KYC for large fiat transfers 
+
+We're truly excited to be announcing this project and can't wait to present more in the coming weeks. To stay up to date, follow our [Twitter](https://twitter.com/SoupyFinance) and join our [Discord](https://discord.gg/2eaQKjVUM3) and [Telegram](https://t.me/+eRMq6BGXVl41ZmRh) servers.
+
+<!--truncate-->
---
+++ docusaurus.config.js
@@ -0,0 +1,116 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Soupy Finance Docs',
+  tagline: 'Soupy Finance Docs',
+  url: 'https://docs.soupy.finance',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'soupy-finance', // Usually your GitHub org/user name.
+  projectName: 'soupy-docs', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/soupy-finance/soupy-docs/',
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Soupy Finance',
+        logo: {
+          alt: 'Soupy Finance Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'home',
+            position: 'left',
+            label: 'Home',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/soupy-finance',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Home',
+                to: '/docs/home',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/2eaQKjVUM3',
+              },
+              {
+                label: 'Telegram',
+                href: 'https://t.me/+eRMq6BGXVl41ZmRh',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/SoupyFinance',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/soupy-finance',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Soupy Finance. Made with fresh ingredients.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
