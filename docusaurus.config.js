65a9cfe6b0c061143e6051a95353153300c6e525
soupy-spook <spook@soupy.finance>
2022-04-18 17:09:17 -0700

    fixed broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ const config = {
             items: [
               {
                 label: 'Home',
-                to: '/docs/home',
+                to: '/',
               },
             ],
           },
